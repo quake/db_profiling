6ff72471db45e79222440032046be9a7eb66e55e
Quake Wang <quake.wang@gmail.com>
2008-06-12 10:18:12 +0800

    move code to cache query

--- lib/db_profiling/db_profiling.rb
+++ lib/db_profiling/db_profiling.rb
@@ -1,59 +1,46 @@
-ActiveRecord::ConnectionAdapters::MysqlAdapter.module_eval do
-  @@stats_queries = @@stats_bytes = @@stats_rows = 0
-
+ActiveRecord::ConnectionAdapters::QueryCache.module_eval do
+  @@stats_hits = []
+  @@stats_misses = []
+  @@stats_bytes = @@stats_rows = 0
+  
   def self.get_stats
-    { :queries => @@stats_queries,
+    { :hits => @@stats_hits,
+      :misses => @@stats_misses,
       :rows => @@stats_rows,
-      :bytes => @@stats_bytes }
+      :bytes => @@stats_bytes,
+      :queries => @@stats_hits.size + @@stats_misses.size,
+    }
   end
+  
   def self.reset_stats
-    @@stats_queries = @@stats_bytes = @@stats_rows = 0
+    @@stats_hits = []
+    @@stats_misses = []
+    @@stats_bytes = @@stats_rows = 0
   end
-
-  def select_with_stats(sql, name)
+  
+  def cache_sql_with_stats(sql, &block)
+    if @query_cache.has_key?(sql)
+      @@stats_hits << sql
+    else
+      @@stats_misses << sql
+    end
     bytes = 0
-    rows = select_without_stats(sql, name)
+    rows = cache_sql_without_stats(sql, &block)
     rows.each do |row|
       row.each do |key, value|
         bytes += key.length
         bytes += value.length if value
       end
     end
-    @@stats_queries += 1
     @@stats_rows += rows.length
     @@stats_bytes += bytes
     rows
-  end
-  alias_method_chain :select, :stats
-end
-
-ActiveRecord::ConnectionAdapters::QueryCache.module_eval do
-  @@hits = []
-  @@misses = []
-
-  def self.get_stats
-    { :hits => @@hits,
-      :misses => @@misses }
-  end
-  def self.reset_stats
-    @@hits = []
-    @@misses = []
-  end
-
-  def cache_sql_with_stats(sql, &block)
-    if @query_cache.has_key?(sql)
-      @@hits << sql
-    else
-      @@misses << sql
-    end
-    cache_sql_without_stats(sql, &block)
   end
   alias_method_chain :cache_sql, :stats
 end
 
 ActionController::Base.module_eval do
   def perform_action_with_reset
-    ActiveRecord::ConnectionAdapters::MysqlAdapter::reset_stats
     ActiveRecord::ConnectionAdapters::QueryCache::reset_stats
     perform_action_without_reset
   end
@@ -61,7 +48,7 @@ ActionController::Base.module_eval do
   alias_method_chain :perform_action, :reset
 
   def active_record_runtime(runtime)
-    stats = ActiveRecord::ConnectionAdapters::MysqlAdapter::get_stats
+    stats = ActiveRecord::ConnectionAdapters::QueryCache::get_stats
     "#{super} #{sprintf("%.1fk", stats[:bytes].to_f / 1024)} queries: #{stats[:queries]}"
   end
 end
--- lib/db_profiling/db_profiling_helper.rb
+++ lib/db_profiling/db_profiling_helper.rb
@@ -2,18 +2,17 @@ module JavaEye
   module DbProfiling
     module Helper
       def db_profiling_view
-        cache_stats = ActiveRecord::ConnectionAdapters::QueryCache.get_stats
-        sql_stats = ActiveRecord::ConnectionAdapters::MysqlAdapter.get_stats
+        stats = ActiveRecord::ConnectionAdapters::QueryCache.get_stats
         <<HTML
   <div id="db_profiling" style="background:pink;color:black;position:absolute;top:16px;left:25%;padding:0.5em;border: 2px solid red;z-index:999;text-align:left;">
       <strong>
-        Queries: <a href="#" onclick="$('cache_hits').toggle();return false;">#{cache_stats[:hits].size}</a> / <a href="#" onclick="$('cache_misses').toggle();return false;">#{cache_stats[:misses].size}</a> / #{number_to_percentage((cache_stats[:hits].size.to_f / (cache_stats[:hits].size + cache_stats[:misses].size)) * 100, :precision => 0)} |
-        Rows: #{sql_stats[:rows]} |
-        Transfer: #{sprintf("%.1fk", sql_stats[:bytes].to_f / 1024)} |
+        Queries: <a href="#" onclick="$('cache_hits').toggle();return false;">#{stats[:hits].size}</a> / <a href="#" onclick="$('cache_misses').toggle();return false;">#{stats[:misses].size}</a> / #{number_to_percentage((stats[:hits].size.to_f / (stats[:queries])) * 100, :precision => 0)} |
+        Rows: #{stats[:rows]} |
+        Transfer: #{sprintf("%.1fk", stats[:bytes].to_f / 1024)} |
         <a href="#" onclick="$('db_profiling').remove();return false;" title="close">[X]</a>
       </strong>
-      <ul id="cache_hits" style="text-align:left;display:none;"><li>#{cache_stats[:hits].join("</li><li>")}</li></ul>
-      <ul id="cache_misses" style="text-align:left;display:none;"><li>#{cache_stats[:misses].join("</li><li>")}</li></ul>
+      <ul id="cache_hits" style="text-align:left;display:none;"><li>#{stats[:hits].join("</li><li>")}</li></ul>
+      <ul id="cache_misses" style="text-align:left;display:none;"><li>#{stats[:misses].join("</li><li>")}</li></ul>
   </div>        
 HTML
       end
